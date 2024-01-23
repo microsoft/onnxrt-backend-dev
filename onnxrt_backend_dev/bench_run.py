6c78fb1c604228b4b8e5639a3685807f8fdb9ed8
Xavier Dupre <xadupre@microsoft.com>
2024-01-23 14:24:08 +0100

    catch errors

--- onnxrt_backend_dev/bench_run.py
+++ onnxrt_backend_dev/bench_run.py
@@ -74,6 +74,11 @@ def run_benchmark(
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         res = p.communicate()
         out, err = res
+        if "ONNXRuntimeError" in res:
+            raise RuntimeError(
+                f"Unable to continue with config {config} due to the "
+                f"following error\n{err}\n----OUTPUT--\n{out}"
+            )
 
         sout = out.decode("utf-8", errors="ignore")
         serr = err.decode("utf-8", errors="ignore")
